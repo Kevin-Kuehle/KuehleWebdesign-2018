abdb39a542a4e5b36b8df95452578047592f0073
Kevin <kevin.kuehle@googlemail.com>
2018-06-05 23:45:45 +0200

    add some files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-import alert from './module/alert.js';
-
 document.addEventListener( 'DOMContentLoaded', function() {
-	alert();
+
 } );
--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
 <?php
 // Add Styles and Scrips
-wp_enqueue_style( 'style', get_stylesheet_uri() );
-wp_enqueue_script( 'default_script', get_template_directory_uri() . '/assets/js/app.min.js', [], '1.0', true );
+
+function kwd_enqueue_my_scripts_and_styles() {
+
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_script( 'default_script', get_template_directory_uri() . '/assets/js/app.min.js', [], '1.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'kwd_enqueue_my_scripts_and_styles' );
