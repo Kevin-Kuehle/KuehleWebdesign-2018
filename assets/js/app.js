e97ce13f5e2464ad7014e92c52b6ff0ac3c98d4d
Kevin <kevin.kuehle@googlemail.com>
2018-08-03 21:00:24 +0200

    add animations checklist, install many pagebuilder and delete it. body to grid and more

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,5 @@
 import mobileNavHandler from './module/mobileMenuHandler';
-import smoothScroll from './module/smoothScroll';
 
 document.addEventListener( 'DOMContentLoaded', function() {
 	mobileNavHandler();
-	smoothScroll();
 } );
--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
-<footer class="footer">
-    <div class="logo">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/logos/logo_w.svg' ?>" alt="Kühle-Webdesign Logo">
-    </div>
-    <div class="footerMenu">
-		<?php wp_nav_menu( [
-			'menu_class'     => 'footerMenu',
-			'container'      => '',
-			'theme_location' => 'footerMenu'
-		] ); ?>
-    </div>
-    <div class="flyToTop"></div>
-</footer>
-<?php wp_footer(); ?>
-</body>
+        <footer class="footer">
+            <div class="gridBox">
+                <div class="logo">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/logos/logo_w.svg' ?>" alt="Kühle-Webdesign Logo">
+                </div>
+                <div class="footerMenu">
+		            <?php wp_nav_menu( [
+			            'menu_class'     => 'footerMenu',
+			            'container'      => '',
+			            'theme_location' => 'footerMenu'
+		            ] ); ?>
+                </div>
+                <div class="optional"></div>
+            </div>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
 </html>
